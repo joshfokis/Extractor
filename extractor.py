f50ca56940edf7dd8765bf3d497bc1706fe3b5ff
Josh <joshfokis@gmail.com>
2018-11-30 00:37:22 -0600

    Update extractor.py
    Add comments to code

--- extractor.py
+++ extractor.py
@@ -3,18 +3,34 @@ import subprocess
 
 
 def main():
+    """
+    this is the main function to extract rar files.
+    """
 
+    # location to walk and get all rar files to extract
     location = input('which folder to extract?\n\n>>>')
 
+    # find all paths directories adn files in the location
+    # requested.
     for path, dirs, files in os.walk(location):
+        # check if a blank file named ignore is or is not
+        # in the folder
         if 'ignore' not in files:
             for file in files:
+                # search for a rar file extention in the
+                # folder
                 if os.path.splitext(file)[-1] == '.rar':
+                    # change directory to extract in the
+                    # same directory
                     os.chdir(path)
+                    # run the command line application unrar
+                    # to extract the file but not to overwrite
                     extract = subprocess.Popen(
                                 ['unrar', 'e', '-o-', file],
                                 stdout=subprocess.PIPE
                                 )
+                    # create the blank ignore file to skip
+                    # the folder on the next run.
                     ignore = subprocess.Popen(
                                 ['touch', 'ignore'],
                                 stdout=subprocess.PIPE
@@ -31,3 +47,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+`
